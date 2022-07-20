72a0ff73ef417debd4f2f2f6f3cf36d56631bca0
Becca Doan <rebecca.doan@cirrus.com>
2022-07-20 11:06:31 -0500

    added db query and UN and PW to env file

--- server.js
+++ server.js
@@ -1,14 +1,33 @@
+require('dotenv').config();
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require('mysql2');
+const db_user = process.env.DB_USER;
+const db_pass = process.env.DB_PASS;
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json())
 
+// Connect to database
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: db_user,
+        password: db_pass,
+        database: 'election'
+    },
+    console.log('Connected to the election database')
+)
 app.get('/', (req, res) => {
     res.json({
         message: 'Hello World'
     })
+})
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
 })
 
 app.use((req, res) => {
