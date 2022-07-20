434a497cd47c5238b82c53971667a3dd9c58e908
Becca Doan <rebecca.doan@cirrus.com>
2022-07-20 11:25:44 -0500

    added queries for creating, deleting, and getting candidates

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json())
 
 // Connect to database
-
 const db = mysql.createConnection(
     {
         host: 'localhost',
@@ -20,15 +19,46 @@ const db = mysql.createConnection(
     },
     console.log('Connected to the election database')
 )
+
+
+
 app.get('/', (req, res) => {
     res.json({
         message: 'Hello World'
     })
 })
 
-db.query(`SELECT * FROM candidates`, (err, rows) => {
-    console.log(rows);
-})
+// Get all candidates
+// db.query(`SELECT * FROM candidates`, (err, rows) => {
+//     console.log(rows);
+// })
+
+// Get a single candidate
+// db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+//     if(err) {
+//         console.log(err);
+//     } else console.log(row);
+// })
+
+// Delete a candidate
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//     if(err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// })
+
+// Create a candidate
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+//             VALUES (?,?,?,?)`;
+// const params = [1, 'Ronald', 'Firbank', 1];
+
+// db.query(sql, params, (err, result) => {
+//     if(err) {
+//         console.log(err);
+//     } else console.log(result);
+// })
+
 
 app.use((req, res) => {
     res.status(404).end();
